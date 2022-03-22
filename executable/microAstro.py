4cb8349db9cd3be5489a3f2424456c404d36ef92
DM-Diane <diane@genielab.co>
2022-03-22 20:07:04 +0100

    specification python libraries

--- executable/microAstro.py
+++ executable/microAstro.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import os
+import platform
 
 os.chdir('../documentation/')
 os.system('python3 -m pip install --upgrade pip')
+os.system('python3 -m pip install --upgrade setuptools')
+os.system('python3 -m pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html')
 os.system('python3 -m pip install -r requirements/python.txt')
 
 os.chdir('../backend/python/')
